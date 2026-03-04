0d649cafe9c1bbbedd56e5ff921e206db7871ad0
leticiamirandacode <leticiamirandavieira@outlook.com>
2026-03-04 15:08:27 -0300

    adicionando exercício

---
+++ calculadora_simples.py
@@ -0,0 +1,8 @@
+print('===Calculadora simples===')
+n1=float(input('Digite um numero: '))
+n2=float(input('Digite outro numero: '))
+soma=n1+n2
+subtracao=n1-n2
+multiplicacao=n1*n2
+divisao=n1/n2
+print(f'Resultados:\n Soma: {soma}\n Subtração: {subtracao}\n Multiplicação: {multiplicacao}\n Divisão: {divisao:.3f}')
